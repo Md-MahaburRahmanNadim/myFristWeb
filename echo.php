b1ca8cb6efc465c07f36ca41bfd289384f76d99e
unknown <mdnadimhaaan120@gmail.com>
2021-03-23 06:18:52 +0600

    add print.py and some change in echo.php and index.html

--- echo.php
+++ echo.php
@@ -1,3 +1,5 @@
 <?php
 
 echo 'My name is Md.Mahabur Rahman Nadim';
+$name = "Md.Mahabur Rahman Nadim";
+echo name;
---
+++ print.py
@@ -0,0 +1,4 @@
+'''
+here we use print to pirnt some thing
+'''
+print('My name is Nadim')
